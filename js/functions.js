493df8b8eafbaeb4dda05d6c83c3af8ac22591b4
Christopher Luby <lub14001@byui.edu>
2018-12-12 13:43:48 -0700

    PushState

--- js/functions.js
+++ js/functions.js
@@ -8,12 +8,18 @@ MENULINK.addEventListener('click', function (evt) {
     console.log(descriptPage);
     if(descriptPage != null){
         evt.preventDefault();
-        // getData(descriptPage);
+        getData(descriptPage);
     }
 });
 // Get Location from weather.json file
 function getData(LOCALE) {
+    if(LOCALE == "Home"){
+        document.getElementById("home-main").setAttribute("class","show");
+        document.getElementById("detail-main").setAttribute("class","hide");
+        return;
+    }
     let URL = "js/detail-info.json";
+    
     console.log(LOCALE);
      fetch(URL)
       .then(response => response.json())
@@ -22,14 +28,24 @@ function getData(LOCALE) {
        console.log(data);
        console.log(data[LOCALE+'']);
        const locData = {}; // Create an object
-       locData['name'] = data[LOCALE+''].City;
-       locData['postal'] = data[LOCALE+''].zip;
-       locData['state'] = data[LOCALE+''].State;
-       locData['geoposition'] = data[LOCALE+''].Latitude + ", " + data[LOCALE+''].Longitude;
-       locData['elevation'] =data[LOCALE+''].Elevation;
+       locData['title'] = data[LOCALE].title;
+       locData['description'] = data[LOCALE].desc;
+    //    locData['state'] = data[LOCALE+''].State;
+    //    locData['geoposition'] = data[LOCALE+''].Latitude + ", " + data[LOCALE+''].Longitude;
+    //    locData['elevation'] =data[LOCALE+''].Elevation;
        console.log(locData);
-       getWeather(locData, LOCALE);
+    //    getWeather(locData, LOCALE);
+        buildPage(locData);
        })
       .catch(error => console.log('There was a getCode error: ', error))
       
     } // end getCode function
+    function buildPage(locData){
+        var stateObj = { foo: "bar" };
+        document.getElementById("page-title").innerHTML = locData.title;
+        document.getElementById("page-descript").innerHTML = locData.description;
+        document.getElementById("home-main").setAttribute("class","hide");
+        document.getElementById("detail-main").setAttribute("class","show");
+        history.pushState(stateObj,"", "#" +locData.title);
+
+    }
