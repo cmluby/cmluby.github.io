0429904693ade930390be466f81f2ae801c983a6
Chris Luby <cmluby@gmail.com>
2023-11-07 22:59:44 -0600

    Formatting and font update

--- js/functions.js
+++ js/functions.js
@@ -6,46 +6,46 @@ MENULINK.addEventListener('click', function (evt) {
     // See https://javascript.info/bubbling-and-capturing for evt.target explanation
     let descriptPage = evt.target.dataset['page'];
     console.log(descriptPage);
-    if(descriptPage != null){
+    if (descriptPage != null) {
         evt.preventDefault();
         getData(descriptPage);
     }
 });
 // Get Location from weather.json file
 function getData(dayinDec) {
-    if(dayinDec == "Home"){
-        document.getElementById("home-main").setAttribute("class","show");
-        document.getElementById("detail-main").setAttribute("class","hide");
+    if (dayinDec == "Home") {
+        document.getElementById("home-main").setAttribute("class", "show");
+        document.getElementById("detail-main").setAttribute("class", "hide");
         return;
     }
     let URL = "js/detail-info.json";
-    
-    console.log(dayinDec);
-     fetch(URL)
-      .then(response => response.json())
-      .then(function (data) {
-       console.log('Json object from getData function:');
-       console.log(data);
-       console.log(data[dayinDec+'']);
-       const dayData = {}; // Create an object
-       dayData['title'] = data[dayinDec].title;
-       dayData['description'] = data[dayinDec].desc;
-    //    dayData['state'] = data[dayinDec+''].State;
-    //    dayData['geoposition'] = data[dayinDec+''].Latitude + ", " + data[dayinDec+''].Longitude;
-    //    dayData['elevation'] =data[dayinDec+''].Elevation;
-       console.log(dayData);
-    //    getWeather(dayData, dayinDec);
-        buildPage(dayData);
-       })
-      .catch(error => console.log('There was a getCode error: ', error))
-      
-    } // end getCode function
-    function buildPage(dayData){
-        var stateObj = { foo: "bar" };
-        document.getElementById("page-title").innerHTML = dayData.title;
-        document.getElementById("page-descript").innerHTML = dayData.description;
-        document.getElementById("home-main").setAttribute("class","hide");
-        document.getElementById("detail-main").setAttribute("class","show");
-        history.pushState(stateObj,"", "#" +dayData.title);
 
-    }
+    console.log(dayinDec);
+    fetch(URL)
+        .then(response => response.json())
+        .then(function (data) {
+            console.log('Json object from getData function:');
+            console.log(data);
+            console.log(data[dayinDec + '']);
+            const dayData = {}; // Create an object
+            dayData['title'] = data[dayinDec].title;
+            dayData['description'] = data[dayinDec].desc;
+            //    dayData['state'] = data[dayinDec+''].State;
+            //    dayData['geoposition'] = data[dayinDec+''].Latitude + ", " + data[dayinDec+''].Longitude;
+            //    dayData['elevation'] =data[dayinDec+''].Elevation;
+            console.log(dayData);
+            //    getWeather(dayData, dayinDec);
+            buildPage(dayData);
+        })
+        .catch(error => console.log('There was a getCode error: ', error))
+
+} // end getCode function
+function buildPage(dayData) {
+    var stateObj = { foo: "bar" };
+    document.getElementById("page-title").innerHTML = dayData.title;
+    document.getElementById("page-descript").innerHTML = dayData.description;
+    document.getElementById("home-main").setAttribute("class", "hide");
+    document.getElementById("detail-main").setAttribute("class", "show");
+    history.pushState(stateObj, "", "#" + dayData.title);
+
+}
