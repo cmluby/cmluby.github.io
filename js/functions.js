fb01e83809f865a8c43af19dfd88a7a4e558f337
cmluby <cmluby@users.noreply.github.com>
2023-11-08 22:48:10 -0600

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -44,7 +44,8 @@ function buildPage(dayData) {
     var stateObj = { foo: "bar" };
     document.getElementById("page-title").innerHTML = dayData.title;
     document.getElementById("page-descript").innerHTML = dayData.description;
-    document.getElementById("form-link").setAttribute("href", dayData.form);
+    var linkEl = document.getElementById("form-link");
+    linkEl.href = dayData.form;
     document.getElementById("home-main").setAttribute("class", "hide");
     document.getElementById("detail-main").setAttribute("class", "show");
     history.pushState(stateObj, "", "#" + dayData.title);
