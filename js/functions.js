84bd9d4e1fb3767c2b5807546a3751bc16225bd4
Chris Luby <cmluby@gmail.com>
2023-11-11 17:37:03 -0600

    Try 3 for image

--- js/functions.js
+++ js/functions.js
@@ -46,7 +46,7 @@ function buildPage(dayData) {
     document.getElementById("page-title").innerHTML = dayData.title;
     document.getElementById("page-descript").innerHTML = dayData.description;
     document.getElementById("form-link").innerHTML = "<a href=\"" + dayData.form + "\" id=\"form-linkdet\">Click here to submit what you did!</a>";
-    document.getElementById("pageimg").setAttribute = ("src", dayData.pic);
+    document.getElementById("pageimg").setAttribute = ("src", dayData.pic + '');
     //linkEl.href = dayData.form;
     document.getElementById("home-main").setAttribute("class", "hide");
     document.getElementById("detail-main").setAttribute("class", "show");
